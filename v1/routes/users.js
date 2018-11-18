373e0392f7dd882fd2934db3b5b770e871c3e57e
Gilles Kagarama <kagaramag@gmail.com>
2018-11-19 00:17:01 +0200

    moved user fx to controller

--- v1/routes/users.js
+++ v1/routes/users.js
@@ -1,121 +1,26 @@
-const express = require('express');
-const Joi = require("joi");
+import express from 'express';
+
 const router = express.Router();
 
-const parcels = require('./../../data/parcels');
-const users = require('./../../data/users');
+import UsersController from '../../controllers/users';
 
-
-
-
-// /GET parcels by user id
-router.get('/:id/parcels', (req, res) => {
-    const id = req.params.id;
-    const result = parcels.filter(c => c.id_client == id);
-    if(result.id_client === id){
-        return result
-    }
-    res.status(200).send({
-        parcels:result
-    });
-});
-
+// get all parcels orders by user id
+router.get('/:id/parcels', UsersController.findParcelsByUserId);
 // GET list of all users
-router.get('/', (req, res, next) =>{ 
-    res.status(200).send({
-        users: users
-    });
-});
+router.get('/', UsersController.findAllUsers);
 
 //GET user details
-router.get('/:id', (req, res, next) =>{
-    const id = parseInt(req.params.id);    
-    users.map((user) => {
-        if(user.id === id){
-            return res.status(200).send({
-                user:user
-            });
-        }
-    });
-    req.setTimeout(500);
-});
+router.get('/:id', UsersController.findUserDetails);
 
 
 //POST new parcel
-router.post('/', (req, res, next) =>{
-    // defining schema
-    const {error} = validateUser(req.body);
-    
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    const user = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        state: 'active',
-        role: 'client',
-        created_time: req.body.created_time
-    };
-    users.push(user);
-    res.send(user);
-    
-    res.send({
-        message:user
-    });
-});
+router.post('/', UsersController.createNewUser);
 
 // make a user inactive
-router.put('/:id/inactive', (req,res) => {
-    // look up user
-    const user = users.find(p => p.id  === parseInt(req.params.id));
-    if(user.state === 'active'){
-        // if not exist, retur 404
-        if(!user) res.status(404).send("Parcel order with given id was not found");
-        // validate, if invalid, return 400 -bad request
-        const { error } = validateUser(req.body);
-        
-        if(error){
-            res.status(400).send(error.details[0].message);
-            return;
-        }
-        // update user
-        user.state = 'inactive';
-        // return the updated user
-        res.send(user);
-    }else{
-        res.send({
-            message: "'" +user.name + "' is already inactive"
-        });
-    }
-});
+router.put('/:id/inactive', UsersController.makeUserInactive);
 
 // make a user active
-router.put('/:id/active', (req,res) => {
-    // look up user
-    const user = users.find(p => p.id  === parseInt(req.params.id));
-    if(user.state === 'inactive'){
-        // if not exist, retur 404
-        if(!user) res.status(404).send("Parcel order with given id was not found");
-        // validate, if invalid, return 400 -bad request
-        const { error } = validateUser(req.body);
-        
-        if(error){
-            res.status(400).send(error.details[0].message);
-            return;
-        }
-        // update user
-        user.state = 'active';
-        // return the updated user
-        res.send(user);
-    }else{
-        res.send({
-            message: "'" +user.name + "' is already inactive"
-        });
-    }
-});
+router.put('/:id/active', UsersController.makeUserActive);
 
 // update user profile info
 // for now, i only update the name of the user
