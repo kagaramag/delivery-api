947f353d49d30ef7a6d1abfb545c7ef9e9ba1172
Gilles Kagarama <kagaramag@gmail.com>
2018-11-23 00:53:32 +0200

    test users http verbs

--- test/users.js
+++ test/users.js
@@ -15,47 +15,17 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 //Our parent block
-describe('Users', () => {
-    // get all parcels /GET /parcels
-    describe('/GET parcels', () => {
-        it('it should GET all users', (done) => {
-          chai.request(app)
-            .get('/api/v1/users')
-            .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                res.body.should.be.a('object');;
+describe('Test Users HTTP Verbs', () => {
+    const id = 1;
+    it(`it should get parcels of users ${id}`, (done) => {
+        chai.request(app)
+        .get(`/api/v1/users/${id}/parcels`)
+        .end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            res.body.parcels.should.be.a("array");
+            res.body.should.be.a('object');;
             done();
-            });
-        });
-    });
-    // POST User
-    describe('/GET users', () => {
-        const id = 1;
-        it(`it should get parcels of users ${id}`, (done) => {
-            chai.request(app)
-            .get(`/api/v1/users/${id}/parcels`)
-            .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                res.body.should.be.a('object');;
-            done();
-            });
-        });
-    });
-    // Get user details id /GET /parcels/:id
-    describe('/GET user details', () => {
-        const id = 1;
-        it(`it should GET a user with id: ${id}`, (done) => {
-            chai
-            .request(app)
-            .get(`/api/v1/users/${id}`)
-            .end((err, res) => {
-                should.not.exist(err);
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-            done();
-            });              
         });
     });
 });
