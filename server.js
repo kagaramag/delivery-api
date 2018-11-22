ccdac4c4fac1087af0e3e3c39f3cf5e7a0e9226b
Gilles Kagarama <kagaramag@gmail.com>
2018-11-22 21:09:06 +0200

    remove spaces

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import http from 'http';
-
 import app from './app';
 
 
-
 const port = process.env.PORT || 3100;
-
 const server = http.createServer(app);
+
 
 server.listen(port, () => {
     console.log(`Server starts on port: ${port}`)
 });
-
