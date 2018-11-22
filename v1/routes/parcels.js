7f722b31f3e8194223f9519231ae91218e27b954
Gilles Kagarama <kagaramag@gmail.com>
2018-11-22 22:13:46 +0200

    add method for new routes

--- v1/routes/parcels.js
+++ v1/routes/parcels.js
@@ -9,12 +9,18 @@ import ParcelsController from './../../controllers/parcels';
 
 // get all parcels orders
 router.get('/', ParcelsController.findAll);
-// get order details
-router.get('/:id', ParcelsController.findOne);
 // cancel order
 router.put('/:id/cancel', ParcelsController.cancelOne);
+// get order details
+router.get('/:id', ParcelsController.findOne);
 // create new parcel order
-router.post('/', ParcelsController.create)
+router.post('/', ParcelsController.create);
+// Set package destination
+// router.put('/:id/destination', ParcelsController.setDestination);
+// // Change package status
+// router.put('/:id/status', ParcelsController.changeStatus);
+// // Change present location
+// router.put('/:id/status', ParcelsController.presentLocation);
 
 // export router
 module.exports = router;
