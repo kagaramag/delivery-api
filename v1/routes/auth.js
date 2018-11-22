adf056faaefdf78c84c1ec676a61f2ee818b40b9
Gilles Kagarama <kagaramag@gmail.com>
2018-11-23 00:56:28 +0200

    auth routes

---
+++ v1/routes/auth.js
@@ -0,0 +1,17 @@
+// require express
+import express from 'express';
+
+// register router
+const router = express.Router();
+
+// import users controller
+import AuthController from './../../controllers/auth';
+
+//POST signup
+router.post('/signup', AuthController.createNewUser);
+
+//POST login
+router.post('/login', AuthController.LoginUser);
+
+
+module.exports = router;
