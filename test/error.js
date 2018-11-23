e9c41e022887566df3f85a3316e53a26b80a638a
Gilles Kagarama <kagaramag@gmail.com>
2018-11-23 02:21:43 +0200

    fixed error

--- test/error.js
+++ test/error.js
@@ -1,15 +1,11 @@
-process.env.NODE_ENV = 'test';
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let app = require('../app');
 let should = chai.should();
-let locations = require('../v1/routes/locations');
-
 
 chai.use(chaiHttp);
-
 
 //Our parent block
 describe('Errors testing', () => {
@@ -22,9 +18,5 @@ describe('Errors testing', () => {
             res.should.have.status(404);
             done();
         })
-    });
-    
-
+    });  
 });
-
-module.exports = app;
