05d68257e2211055b05c4283d00c27707fc91031
Gilles Kagarama <kagaramag@gmail.com>
2018-11-25 13:26:44 +0200

    config db

---
+++ db/config.js
@@ -0,0 +1,11 @@
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'gilles',
+  host: 'localhost',
+  database: 'sendit',
+  password: 123123,
+  port: 5432,
+});
+
+export default pool;
