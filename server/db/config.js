a9ca85a39a6bf36b9af10c47534658ab03841191
Gilles Kagarama <kagaramag@gmail.com>
2018-11-29 17:37:45 +0200

    fix db bug

--- server/db/config.js
+++ server/db/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   user:process.env.USER_DB,
   host: 'localhost',
-  database: process.env.DATABASE || gilles,
+  database: process.env.DATABASE || sendit,
   password:process.env.PASSWORD_DB,
   port: 5432
 });
