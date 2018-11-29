3ac368d4de6baf3db8044d794b1f1325e2bcf657
Gilles Kagarama <kagaramag@gmail.com>
2018-11-29 14:25:34 +0200

    db config uses .env

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,10 @@
 const { Pool, Client } = require('pg')
 require('dotenv').config();
+
 const pool = new Pool({
   user:process.env.USER_DB ,
   host: 'localhost',
-  database: 'sendit',
+  database: process.env.DATABASE || postges,
   password:process.env.PASSWORD_DB,
   port: 5432
 });
