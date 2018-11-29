445278fc2416307155c9ed5fc6e0775fbf1ab975
Gilles Kagarama <kagaramag@gmail.com>
2018-11-29 20:01:38 +0200

    heroku config

--- server/db/config.js
+++ server/db/config.js
@@ -1,12 +1,32 @@
 const { Pool, Client } = require('pg')
 require('dotenv').config();
 
-const pool = new Pool({
-  user:process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password:process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
-});
 
+let config;
+if(process.env.NODE_ENV === 'production'){ 
+  const url = require('url');
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+   
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+   
+}else if(process.env.NODE_ENV === 'test'){
+  config = {
+    user:process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+  };
+
+}
+console.log(config);
+const pool = new Pool(config);
 export default pool;
